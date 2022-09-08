9632eae01d1a4394c90123053b4e4caff7d4f230
Replit user <>
2022-09-08 03:07:44 -0500

    fixed adding shared user id to array list of todo item

--- controllers/todos.js
+++ controllers/todos.js
@@ -35,64 +35,29 @@ module.exports = {
         }
     },
     shareTodo: async (req, res) => {
-        // console.log(req.user)
-        // console.log(req.user.userName)
-        // try {
-        //     const sharedItems = await Todo.find({
-        //         userId: req.user.id
-        //     })
-        //     const shareCreator = req.user.userName
-        //     const shareCreatorId = req.user._id // MongoDb User Id for creator of the share
-        //     const shareRecipient = req.body.shareReceiver // userName used to search DB from form
-        //     // const dbObjectId = this.parentNode.dataset.id // id from the DOM for a todo item
-        //     // const dbObjectData = await User.findById(dbObjectId)
-        //     // const recipientData = await User.findOne({
-        //     //     userName: shareRecipient
-        //     // })
-        //     // const recipientId = recipientData._id
-        //     // const dbObjectUserArray = dbObjectData.userId
-        //     console.log(`${req.body.todoIdFromJSFile} !!!!!!!`)
-        //     // console.log(JSON.stringify(this.parentNode))
-
-        //     // console.log(`${dbObjectUserArray} ????????`)
-
-        //     // function to search the DB and find a userName that matches input from form, responds with entire user object
-        //     // *** just for now, we are searching by userId, but will later search by userName
-        //     // creating a todo for the recipient of the todo
-        //     // await Todo.create({
-        //     //     todo: itemText, // this is a test todo item will change later
-        //     //     completed: false,
-        //     //     userId: recipientData._id, // Mongo DB Id for recipient
-        //     //     sharedId: shareCreatorId // Added a share creator id for later use
-        //     // })
-        //     // await Todo.findOneAndUpdate(
-        //     //     { _id: dbObjectId },
-        //     //     { $push: { userId: { recipientId } } }
-        //     // )
-        //     // console.log(
-        //     //     `${shareCreator} has shared ${todo} todo with ${shareRecipient}`
-        //     // )
-        //     res.redirect('/todos')
-        const shareRecipient = req.body.shareReceiver // userName used to search DB from form
-        const dbObjectId = req.body.todoIdFromJSFile // id from the DOM for a todo item
-        // const dbObjectData = await User.findById(dbObjectId)
-        const recipientData = await User.findOne({
-            userName: shareRecipient
-        })
-        let recipientId = recipientData._id
-        recipientId = String(recipientId)
-
         try {
-            console.log(recipientData)
-            console.log(recipientId)
-            console.log(typeof recipientId)
-
-            await Todo.findOneAndUpdate(
-                { _id: req.body.todoIdFromJSFile }, // id of the todo
-                { $push: { userId: { recipientId } } }
-            )
-            console.log('Task Shared')
-            res.json('Task Shared')
+            // find the User in db to share task with
+            const shareRecipient = await User.findOne({
+                userName: req.body.shareReceiver // userName that is sent with the form
+            })
+            const dbObjectId = req.body.todoId // todo (task) item ID that is sent with the form
+            let todo = await Todo.findById(dbObjectId)
+            // if user id already in the userId array do not add userId to array
+            if (todo.userId.includes(shareRecipient._id)) {
+                console.log('Task already shared')
+            } else {
+                await Todo.findOneAndUpdate(
+                    { _id: dbObjectId },
+                    {
+                        // push the user's ID that you want to share with to the userId array
+                        $push: {
+                            userId: shareRecipient._id // UserId of the share recipient
+                        }
+                    }
+                )
+                console.log('Task Shared')
+            }
+            res.redirect('/todos')
         } catch (err) {
             console.log(err)
         }
